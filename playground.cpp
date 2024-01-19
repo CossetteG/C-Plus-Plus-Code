a7ff5075f346a98b89cf79ee12419f2348004654
Cossette Gomez <10838257@my.uvu.edu>
2024-01-18 23:03:26 -0700

    trying for hello world

--- playground.cpp
+++ playground.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
 
 int main() {
-    cout << "Hello World";
-    cout << endl;
+    int i;
+    cout << "Hello World blerggggg" << endl;
 
     return 0;
 }
