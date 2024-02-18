f07274ea5c7537f26411cf5f2f08ad7e29b39da3
Cossette Gomez <10838257@my.uvu.edu>
2024-02-17 21:58:53 -0700

    Chapters 3 4 5 Notes

--- PracticeArchive.cpp
+++ PracticeArchive.cpp
@@ -133,3 +133,186 @@ int main() {
 
    return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int highwayNumber;
+   
+   cin >> highwayNumber;
+
+   if (highwayNumber < 1 || highwayNumber > 999 || highwayNumber % 100 == 0) {
+      cout << highwayNumber << " is not a valid interstate highway number." << endl;
+   }
+   else {
+      cout << "I-" << highwayNumber << " is ";
+      
+      if (highwayNumber > 100) {
+         cout << "auxiliary, ";
+         
+         int serveNumber;
+         serveNumber = highwayNumber % 100;
+         cout << "serving I-" << serveNumber;
+      }
+      else {
+         cout << "primary";
+      }
+      
+      string direction;
+      direction = (highwayNumber % 2 == 0)? "east/west.":"north/south.";
+      cout << ", going " << direction << endl;
+   }   
+
+   return 0;
+}
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string firstN;
+   string middleN;
+   string lastN;
+   
+   cin >> firstN;
+   cin >> middleN;
+   cin >> lastN; 
+   
+   if (lastN == "") {
+      lastN = middleN;
+      middleN = "";
+   }
+   
+   /*
+   cout << firstN << endl;
+   cout << middleN << endl;
+   cout << lastN << endl;
+   */
+   
+   char firstInit;
+   char middleInit;
+   
+   firstInit = firstN.at(0);
+   
+   cout << lastN << ", " << firstInit << ". ";
+   
+   if (middleN != "") {
+      middleInit = middleN.at(0);
+      cout << middleInit << '.';
+   }
+   
+   cout << endl;
+
+   return 0;
+}
+
+#include <bitset>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the integer that holds the bits: ";
+    unsigned int n;
+    cin >> n;
+
+    cout << "Enter the rightmost bit position: ";
+    unsigned int k;
+    cin >> k;
+
+    cout << "Enter the leftmost bit position: ";
+    unsigned int m;
+    cin >> m;
+    
+    unsigned int sol;
+    sol = (n & (((1 << (m-k+1)) - 1) << k)) >> k;
+    
+    cout << "Extracting bits " << k << " through " << m << " from ";
+    cout << n << " = " << bitset<32>(n) << ':' << endl;
+    cout << "Result = " << sol << " = " << bitset<32>(sol) << endl;
+
+}
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+   string userin;
+   string userinCOPY;
+   int inputLength;
+   int halfLength;
+   bool isPalindrome;
+   string outputstr;
+   int i;
+   
+   isPalindrome = true;
+   getline(cin, userin);
+   inputLength = userin.size();
+   userinCOPY = userin;
+   
+   while (userin.find(' ') != string::npos) {
+      int itemIndex;
+      itemIndex = userin.find(' ');
+      userin.replace(itemIndex, 1, "");
+   }
+   
+   inputLength = userin.size();
+   halfLength = inputLength / 2;
+   
+   for (i=0; i < halfLength; ++i) {
+      int endchar;
+      endchar = inputLength - i -  1;
+      //cout << userin.at(i) << ' ' << userin.at(endchar) << endl;
+      
+      if (userin.at(i) == userin.at(endchar)) {
+         continue;
+      }
+      
+      isPalindrome = false;
+   }
+   
+   outputstr = (isPalindrome)? "palindrome: ":"not a palindrome: ";
+   //cout << inputLength << ' ' << halfLength;
+   cout << outputstr << userinCOPY << endl;
+
+   return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int width;
+   int height;
+   int arrow;
+   int i;
+   int j;
+   
+   cin >> height;
+   cin >> width;
+
+   do {
+      cin >> arrow;
+   } while (arrow <= width);
+   
+   for (i=0; i < height; ++i) {
+      for (j=0; j < width; ++j) {
+         cout << '*';
+      }
+      cout << endl;
+   }
+   
+   for (arrow; arrow >= 0; --arrow) {
+      for (j=0; j<arrow; ++j) {
+         cout << '*';
+      }
+      if (arrow > 0) {
+      cout << endl;
+      }
+   }
+
+   return 0;
+}
