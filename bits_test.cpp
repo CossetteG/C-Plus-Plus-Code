eb51b5e0969b0a535ad075b84b6c81d1034e4e82
Cossette Gomez <10838257@my.uvu.edu>
2024-03-23 19:51:26 -0600

    idk run please

---
+++ bits_test.cpp
@@ -0,0 +1,16 @@
+
+//Bits myBits;
+//myBits = Bits(10);
+//std::cout << "bits" << std::endl;
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    int i;
+    std::cout << "Hello World boogie oogie" << endl;
+
+    return 0;
+}
