8c6387ab85fd042d9ff4b4b39112b90019f85401
Cossette Gomez <10838257@my.uvu.edu>
2024-03-27 01:16:00 -0600

    bits

--- bits.h
+++ bits.h
@@ -1,9 +1,10 @@
-#ifndef BITS_H
-#define BITS_H
 
 #include <bitset>
 #include <cassert>
 #include <cmath>
+// #include <iostream>
+/*Student note: iostream was not working on my machine for some reason, 
+so nothing using ostream or cout has been tested. <3 Cossette*/
 
 class Bits {
     using IType = unsigned long long; // We only have to change the integer type here, if desired
@@ -14,6 +15,7 @@ public:
     Bits(IType n) {
         bits = n;
     }
+    
     static int size() {
         return NBITS;
     }
@@ -21,20 +23,91 @@ public:
         assert(0 <= pos && pos < NBITS);
         return bits & (IType(1) << pos);
     }
-    // void set(int pos) {                   // Sets the bit at position pos
-    //     assert(0 <= pos && pos < NBITS);
-    //     this->bits += 1; //(pow(2, pos));
-    // };       
-    void set();               // Sets all bits
-    void reset(int pos);      // Resets (makes zero) the bit at position pos
-    void reset();             // Resets all bits
-    void assign(int pos, bool val); // Sets or resets the bit at position pos depending on val
-    void assign(IType n);     // Replaces the underlying integer with n
-    void toggle(int pos);     // Flips the bit at position pos
-    void toggle();            // Flips all bits
-    void shift(int n);        // If n > 0, shifts bits right n places; if n < 0, shifts left
-    void rotate(int n);       // If n > 0, rotates right n places; if n < 0, rotates left
-    int ones() const;         // Returns how many bits are set in the underlying integer
+    void set(int pos) {                   // Sets the bit at position pos
+        assert(0 <= pos && pos < NBITS);
+        // if (this->at(pos) == 0) {
+        //     IType add = (pow(2, (pos)));
+        //     this->bits += add; };
+
+        this->bits = (this->bits | IType(1) << pos);
+    };       
+    void set() {                           // Sets all bits
+        IType total = 0;
+        for (int i=0; i<NBITS; i++) {
+            IType add = (pow(2, (i)));
+            total += add;
+        }
+        assert(total != 0);
+        this->bits = total;
+    };               
+    void reset(int pos){            // Resets (makes zero) the bit at position pos
+        assert(0 <= pos && pos < NBITS);
+        if (this->at(pos) == 1) {
+        //     IType subtr = (pow(2, (pos)));
+        //     this->bits -= subtr; } ;
+            IType r = pow(2, (pos));
+            this->bits = this->bits ^ r;
+        }
+    };      
+    void reset() {                  // Resets all bits
+        this->bits = 0;
+    };             
+    void assign(int pos, bool val) {    // Sets or resets the bit at position pos depending on val
+        assert(0 <= pos && pos < NBITS);
+        if (val == true) {
+            this->set(pos);
+        }else {
+            this->reset(pos);
+        }
+    }; 
+    void assign(IType n) {      // Replaces the underlying integer with n
+        this->bits = n;
+    };     
+    void toggle(int pos){       // Flips the bit at position pos
+        assert(0 <= pos && pos < NBITS);
+        if (this->at(pos) == 1) {this->reset(pos);}
+        else{this->set(pos);}
+        
+    };     
+    void toggle() { // Flips all bits
+        this->bits = ~(this->bits);
+    };            
+    void shift(int n) {     // If n > 0, shifts bits right n places; if n < 0, shifts left
+        int m = -1*n;
+        if (n > 0) {
+            this->bits = this->bits<<n;
+        } else {
+            this->bits = this->bits>>m;
+        }
+    }; 
+    void rotate(int n) {   // If n > 0, rotates right n places; if n < 0, rotates left
+        n = n % NBITS;
+        int m = -1*n;
+        IType r = 0;
+
+        if (n > 0) {
+            for (int i=0; i<n; i++) {
+                if (this->at(NBITS - i-1) == 1) { 
+                r += pow(2, (NBITS - i-1));} }
+            r = r >> (NBITS - n);
+            this->shift(n);
+            this->bits = this->bits | r;
+        } else if (n < 0) {
+            for (int i=0; i<m; i++) {
+                if (this->at(i) == 1) { 
+                r += pow(2, (i));}}
+            r = r << (NBITS - m);
+            this->shift(n);
+            this->bits = this->bits | r;
+        } else {}
+    }; 
+    int ones() const{        // Returns how many bits are set in the underlying integer
+        int total = 0;
+        for (int i=0; i>NBITS; i++) {
+            if (this->at(i) == 1) { total += 1;}
+        }
+        return total;
+    }
     int zeroes() const {      // Returns how many bits are reset in the underlying integer
         return NBITS - ones();
     }
@@ -47,11 +120,9 @@ public:
     friend bool operator!=(const Bits& b1, const Bits& b2) {
         return b1.bits != b2.bits;
     }
-    friend std::ostream& operator<<(std::ostream& os, const Bits& b) {
-        return os << std::bitset<NBITS>(b.bits);    // Be sure to #include <bitset>
-    }
+    // friend std::ostream& operator<<(std::ostream& os, const Bits& b) {
+    //     return os << std::bitset<NBITS>(b.bits);    // Be sure to #include <bitset>
+    // }
 };
+  
 
-
-
-#endif
--- bits_test.cpp
+++ bits_test.cpp
@@ -1,5 +1,5 @@
 
-// #include <ostream>
+// #include <iostream>
 #include <windows.h>
 #include "bits.h"
 
@@ -7,10 +7,11 @@ using namespace std;
 
 void seeBitValues(int places, Bits bits) {
     int j = 0;
-    for (int i; i<=6; i++) {
+
+    for (int i=0; i<places; i++) {
         j = bits.at(i);
         assert((j == 0) || (j == 1));
-        //cout << j << endl;
+        // cout << j << ' ' << endl;
     }
 }
 
@@ -20,8 +21,45 @@ int main() {
 
     assert(myBits.size() == 64);
 
-
+    myBits.set(2);      //pos count starts at 0 ---- bits = 14
     seeBitValues(4, myBits);
+
+    myBits.reset(1);    // bits = 12
+    seeBitValues(4, myBits);
+
+    myBits.set();       // bits = 18somethingorothernumber
+    seeBitValues(4, myBits);
+
+    myBits.reset();     // bits = 0
+    seeBitValues(4, myBits);
+
+    myBits.assign(14);  // bits = 14
+    seeBitValues(10, myBits);
+
+    myBits.assign(0, true); //bits = 15
+    seeBitValues(10, myBits);
+
+    myBits.assign(2, false); //bits = 11
+    seeBitValues(10, myBits);
+
+    myBits.assign(2);
+    seeBitValues(5, myBits);
+
+    myBits.shift(1);   // bits = 4
+    seeBitValues(5, myBits);
+
+    myBits.shift(-2); // bits = 1
+    seeBitValues(5, myBits);
+
+    myBits.rotate(-2); //bits = 46whatever
+    seeBitValues(5, myBits);
+
+    myBits.rotate(3); //bits = 2
+    seeBitValues(5, myBits);
+
+    myBits.assign(10);
+    myBits.toggle(1);   //bits = 8
+    seeBitValues(5, myBits); 
 
     return 0;
 }
